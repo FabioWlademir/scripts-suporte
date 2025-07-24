76214b5604cea60288ee0e58aa2f52007c2c920c
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 13:54:33 -0300

    Update limpar_cache_winfows_update.bat

--- bat-windows/manutencao/limpar_cache_winfows_update.bat
+++ bat-windows/manutencao/limpar_cache_winfows_update.bat
@@ -1,1 +1,11 @@
+@echo off
+:: Script para limpar o cache do Windows Update
+:: Autor: Fábio Wlademir
 
+net stop wuauserv
+net stop bits
+rd /s /q %windir%\SoftwareDistribution
+net start wuauserv
+net start bits
+echo Cache do Windows Update limpo.
+pause
