9082b6f9593c16295fadd74257ef0cdb3a70c130
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 16:43:37 -0300

    Create informacoes_sistema.sh

---
+++ shell-linux/sistema /informacoes_sistema.sh
@@ -0,0 +1,7 @@
+# ===========================================
+# Script: Informacoes sistema
+# Autor: Fábio Wlademir
+# ===========================================
+
+#!/bin/bash
+uname -a && lsb_release -a
