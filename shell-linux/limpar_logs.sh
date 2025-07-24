7371db5add6a1b274ae58e806c1d9c6f48635922
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 17:49:19 -0300

    Delete shell-linux/limpar_logs.sh

--- shell-linux/limpar_logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script para limpar logs antigos do sistema
-# Autor: Fábio Wlademir
-
-echo "Removendo logs com mais de 7 dias..."
-find /var/log -type f -mtime +7 -exec rm -f {} \;
-echo "Logs antigos removidos."
