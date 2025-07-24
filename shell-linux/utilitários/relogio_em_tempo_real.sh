839aa8a3d61942f10d21c195c2a792371ca1017e
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 17:37:03 -0300

    Create relogio_em_tempo_real.sh

---
+++ shell-linux/utilitários/relogio_em_tempo_real.sh
@@ -0,0 +1,7 @@
+# ===========================================
+# Script: Relogio em tempo real
+# Autor: Fábio Wlademir
+# ===========================================
+
+#!/bin/bash
+watch -n 1 date
