fff76f1006f098dfa6d8954362ee83fb570b114f
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 13:34:57 -0300

    Update mapear_unidade_rede.bat

--- bat-windows/redes/mapear_unidade_rede.bat
+++ bat-windows/redes/mapear_unidade_rede.bat
@@ -1,1 +1,14 @@
 
+@echo off
+:: Script para mapear uma unidade de rede
+:: Autor: Fábio Wlademir
+
+:: Solicita letra da unidade e caminho da pasta de rede
+set /p letra=Digite a letra da unidade (ex: Z): 
+set /p caminho=Digite o caminho da rede (ex: \\servidor\compartilhamento): 
+
+:: Mapeia a unidade
+net use %letra%: %caminho% /persistent:yes
+
+echo Unidade de rede %letra% mapeada para %caminho%.
+pause
