5f3c31908aedec4492ffe7f50420099eef1c42d0
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 16:51:38 -0300

    Create listar_usuarios_admin.ps1

---
+++ powershell/segurança /listar_usuarios_admin.ps1
@@ -0,0 +1,6 @@
+# ===========================================
+# Script: Listar usuarios admin
+# Autor: Fábio Wlademir
+# ===========================================
+
+Get-LocalGroupMember -Group "Administradores"
