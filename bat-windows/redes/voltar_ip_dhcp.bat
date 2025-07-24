948913217d8f86d292379a01ba4e517506c8a96e
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-07-24 13:45:22 -0300

    Update voltar_ip_dhcp.bat

--- bat-windows/redes/voltar_ip_dhcp.bat
+++ bat-windows/redes/voltar_ip_dhcp.bat
@@ -1,1 +1,8 @@
+@echo off
+:: Script para configurar IP e DNS para DHCP
+:: Autor: Fábio Wlademir
 
+netsh interface ip set address "Ethernet" dhcp
+netsh interface ip set dns "Ethernet" dhcp
+echo IP e DNS configurados para DHCP.
+pause
