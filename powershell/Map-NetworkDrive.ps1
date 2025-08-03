18a3650fdcbe0d1096f02c9264f60bec0da7c03d
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-08-03 19:37:56 -0300

    Delete powershell/Map-NetworkDrive.ps1

--- powershell/Map-NetworkDrive.ps1
+++
@@ -1,5 +0,0 @@
-# Script para mapear unidade de rede
-# Autor: Fábio Wlademir
-
-New-PSDrive -Name "Z" -PSProvider FileSystem -Root "\\servidor\compartilhamento" -Persist
-Write-Output "Unidade Z: mapeada com sucesso."
