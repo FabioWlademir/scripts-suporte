083d34460047f4d7dff43339df29d818c6401ff3
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-08-03 19:09:59 -0300

    Delete powershell/manutenção  directory

--- powershell/manutenção /limpar_eventos.ps1
+++
@@ -1,9 +0,0 @@
-# ===========================================
-# Script: Limpar eventos
-# Autor: Fábio Wlademir
-# ===========================================
-
-wevtutil cl System
-wevtutil cl Application
-wevtutil cl Security
-Write-Host "Logs de eventos limpos com sucesso."
