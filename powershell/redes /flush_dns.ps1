b1caaed8371333a78ea5928b20e99f56f32ffeb3
Fábio Wlademir <138263067+FabioWlademir@users.noreply.github.com>
2025-08-03 19:13:03 -0300

    Create flush_dns.ps1

---
+++ powershell/redes /flush_dns.ps1
@@ -0,0 +1,10 @@
+<#
+.SINOPSE
+Limpa o cache DNS local — útil após alteração de registros ou migração de domínios.
+
+.AUTOR
+Fábio Wlademir
+#>
+
+Clear-DnsClientCache
+Write-Host "🧠 Cache DNS local limpo com sucesso."
